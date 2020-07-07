9e814f96669908254042ce241520160afcd6552a
Scott G <scottg489@gmail.com>
2020-07-07 14:49:49 -0700

    Don't use the 4th parameter for Twilio secrets
    While copy pasting these parameters we were using the 4th argument to
    the build script for all of the Twilio secrets when they should have
    been incrememted in order.
    
    - Fix typo in parameter name

--- token_server/build.sh
+++ token_server/build.sh
@@ -1,5 +1,5 @@
 curl -v -sS -w '%{http_code}' \
-  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'", "TWILIO_ACCOUNT_SI": "'"$4"'", "TWILIO_API_KEY": "'"$4"'", "TWILIO_API_SECRET": "'"$4"'", "TWILIO_CHAT_SERVICE_SID": "'"$4"'"}' \
+  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "AWS_CREDENTIALS": "'"$3"'", "MAIN_KEY_PAIR": "'"$4"'", "TWILIO_ACCOUNT_SID": "'"$5"'", "TWILIO_API_KEY": "'"$6"'", "TWILIO_API_SECRET": "'"$7"'", "TWILIO_CHAT_SERVICE_SID": "'"$8"'"}' \
   'http://simple-ci.com/build?image=scottg489/debatable-token-server-build:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
