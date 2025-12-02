05de1b7415efe257fd866a9e921ae0a88c684b93
Scott Giminiani <scottg489@gmail.com>
2025-12-01 23:51:32 -0500

    Use guard clause for git clone

--- infra/build/run.sh
+++ infra/build/run.sh
@@ -18,9 +18,7 @@ declare -r _TFSTATE_BUCKET_NAME='tfstate-debatable'
 declare -r _RUN_TASK=$(jq -r .RUN_TASK <<< "$1")
 declare -r _GIT_BRANCH=$(jq -r .GIT_BRANCH <<< "$1")
 
-if [ ! -d "$_PROJECT_NAME" ]; then
-  git clone --branch $_GIT_BRANCH $_GIT_REPO
-fi
+[ -d "$_PROJECT_NAME" ] || git clone --branch $_GIT_BRANCH $_GIT_REPO
 cd $_PROJECT_NAME
 
 build_application
